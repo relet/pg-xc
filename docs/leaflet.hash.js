20b432f194d0ae0875dadd5797b5d6f166401edc
Thomas Hirsch <thomas.hirsch@celerway.com>
2018-10-02 00:22:05 +0200

    revert layer ordering tests

--- docs/leaflet.hash.js
+++ docs/leaflet.hash.js
@@ -18,12 +18,11 @@
 			hash = hash.substr(1);
 		}
 		var args = hash.split("/");
-		if (args.length > 2) {
+		if (args.length == 4) {
 			var zoom = parseInt(args[0], 10),
-			lat = parseFloat(args[1]);
-			lon = parseFloat(args[2]);
-      var layers = null;
-      if (args[3]) layers = (args[3]).split("-");
+			lat = parseFloat(args[1]),
+			lon = parseFloat(args[2]),
+			layers = (args[3]).split("-");
 			if (isNaN(zoom) || isNaN(lat) || isNaN(lon)) {
 				return false;
 			} else {
@@ -44,6 +43,7 @@
 		    precision = Math.max(0, Math.ceil(Math.log(zoom) / Math.LN2)),
 		    layers = [];
 
+		//console.log(this.options);
 		var options = this.options;
 		//Check active layers
 		for(var key in options) {
@@ -122,17 +122,15 @@
 				var layers = parsed.layers,
 					options = this.options,
 					that = this;
+				//Add/remove layers
+				this.map.eachLayer(function(layer) {
+					that.map.removeLayer(layer);
+				});
 
-        if (layers) {
-  				//Add/remove layers
-  				this.map.eachLayer(function(layer) {
-  					that.map.removeLayer(layer);
-  				});
-
-          for (var i=0, len=layers.length; i<len; i++) {
-		  			that.map.addLayer(options[layers[i]]);
-			  	};
-        }
+				layers.forEach(function(element, index, array) {
+					//console.log(options[element]);
+					that.map.addLayer(options[element]);
+				});			
 
 				this.movingMap = false;
 			} else {
